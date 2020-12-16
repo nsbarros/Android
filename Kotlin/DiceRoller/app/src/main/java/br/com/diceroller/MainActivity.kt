f3496dd225861875cdcc6299dfb97ab9f4aae63d
Nathan Barros <nathansouzabarros@gmail.com>
2020-12-16 09:44:23 -0300

    Implementation of dice images

--- Kotlin/DiceRoller/app/src/main/java/br/com/diceroller/MainActivity.kt
+++ Kotlin/DiceRoller/app/src/main/java/br/com/diceroller/MainActivity.kt
@@ -1,23 +1,41 @@
 package br.com.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceImage: ImageView
+    private lateinit var rollButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var rollButton = roll_button
+
+        diceImage = dice_image
+        rollButton = roll_button
+
         rollButton.setOnClickListener {
             rollDice()
         }
     }
 
+
     private fun rollDice() {
-        roll_text.text = (1..6).random().toString()
+        var index = (1..6).random()
+
+        val drawableResourceImage = when (index) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImage.setImageResource(drawableResourceImage)
     }
 }
